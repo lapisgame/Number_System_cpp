c9313dc9a2d1af65e23a21f502d6a108c18db9c3
lapis <klimantovartem@gmail.com>
2023-12-26 01:31:50 +0300

    Multiply table

---
+++ Table_multiply.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//^ Переводит inp_znach из 10-чной в new_base-чную 
+//^ с точностью после запятой в accurancy знаков
+string iz_10_v_P(double inp_znach, int new_base, int accurancy=9){
+    string alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    string new_znach="", new_drob="";
+    int old_znach = (int) inp_znach;
+    double old_drob = inp_znach - old_znach;
+
+    while (old_znach > 0){
+        new_znach = alphabet[old_znach % new_base] + new_znach;
+        old_znach = old_znach / new_base;
+    }
+
+
+    int lencount = 0;
+    while ((lencount < accurancy) and (old_drob != 0)){
+        new_drob += alphabet[(int)(old_drob * new_base)];
+        old_drob = (old_drob * new_base) - (int)(old_drob * new_base);
+        lencount += 1;
+    }
+
+    if (new_znach == ""){
+        new_znach = "0";
+    }
+    if (new_drob == ""){
+        new_drob = "0";
+    }
+
+    return new_znach + "." + new_drob;
+}
+
+int main(){
+    int p;
+    cout << "P = ";
+    cin >> p;
+
+    cout << "\t\t";
+    for (int i=0; i<p; i++){
+        cout<<i<<"\t";
+    }
+    cout<<endl;
+    
+    cout <<"\t";
+    for (int i=0; i<p; i++){
+        cout<<"---------";
+    }
+    cout<<endl;
+
+    for (int i=0; i<p; i++){
+        cout<<i<<"\t"<<"|"<<"\t";
+        for (int j=0; j<p; j++){
+            cout<<iz_10_v_P(i*j, p)<<"\t";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
